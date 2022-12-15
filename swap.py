f97c0b99f5d1a29d83ef981f0a3ff9941f78be25
Tom <tomas@humminghomes.com>
2022-12-15 18:34:09 -0300

    feat: Update concepts generator and upload bbva extract

--- swap.py
+++ swap.py
@@ -1,21 +1,27 @@
+import re
 from csv import DictReader, DictWriter
 
 
-def read_supervielle_flow():
-    with open('extractos/supervielle_flow.csv', newline='\r\n') as sup:
+def read_extract(extract_name):
+    with open(f'swap/extractos/{extract_name}.csv', newline='\r\n') as sup:
         file = sup.readlines()
         data = DictReader(file, delimiter=';')
         data = [*data]
         return data
 
 if __name__ == '__main__':
-    extract = read_supervielle_flow()
-    conceptos_set = {line['Concepto'] for line in extract}
-    with open('conceptos/supervielle.csv', 'w', newline='\n') as conceptos:
-        fieldnames=['conceptos']
-        csv = DictWriter(conceptos, fieldnames=fieldnames)
-        csv.writeheader()
-        for concepto in conceptos_set:
-            csv.writerow({'conceptos': concepto})
+    extracts = {
+        'supervielle': {'data': read_extract('supervielle_flow')},
+        'bbva': {'data': read_extract('bbva')}
+    }
     
-    print("Done extracting curated concepts")
+    for bank in extracts:
+        with open(f'swap/conceptos/{bank}.csv', 'w', newline='\n') as conceptos:
+            fieldnames=['conceptos']
+            csv = DictWriter(conceptos, fieldnames=fieldnames)
+            csv.writeheader()
+            conceptos_set = {re.sub(r'(\d+/\d+/\d+)|(\d{3,})', '', line['Concepto']).strip() for line in extracts[bank]['data']}
+            for concepto in conceptos_set:
+                csv.writerow({'conceptos': concepto})
+    
+    print(f"Done extracting curated concepts for {extracts.keys}")
